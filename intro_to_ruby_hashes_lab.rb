2f3d8ffa1306c8987ac34c1be2c2eaf04b44a036
Nils van der Werf <n.vanderw.92@gmail.com>
2022-03-09 09:27:51 +1100

    Create all nested tiers

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,101 +1,63 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-  monopoly = Hash.new
-  monopoly[:railroads] = {}
-  monopoly
+  monopoly = {
+    :railroads => {}
+  }
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
-  monopoly = Hash.new
-  monopoly[:railroads] = {}
-
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:rent_in_dollars] = {}
-  monopoly[:railroads][:names] = {}
-  monopoly
-
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
+    }
+  }
 end
 
-
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-
-  monopoly = Hash.new
-  monopoly[:railroads] = {}
-
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:rent_in_dollars] = {}
-  monopoly[:railroads][:names] = {}
-
-  # Updating "Rent in dollars" key
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-
-  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] * 2
-
-  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 
-  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] * 2
-
-  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 
-  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] * 2
-
-  # Updating "Names" key
-  monopoly[:railroads][:names][:reading_railroad] = {}
-  monopoly[:railroads][:names][:pennsylvania_railroad] = {}
-  monopoly[:railroads][:names][:b_and_o_railroad] = {}
-  monopoly[:railroads][:names][:shortline_railroad] = {}
-  
-  pp monopoly
-  monopoly
-
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline_railroad => {}
+      }
+    }
+  }
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  monopoly = Hash.new
-  monopoly[:railroads] = {}
-
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:rent_in_dollars] = {}
-  monopoly[:railroads][:names] = {}
-
-  # Updating "Rent in dollars" key
-  monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
-
-  monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
-
-  monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
-
-  monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
-
-  # Updating "Names" key
-  monopoly[:railroads][:names][:reading_railroad] = {
-    mortgage_value: 100
+  monopoly = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
+      :names => {
+        :reading_railroad => {
+          :mortgage_value => 100
+        },
+        :pennsylvania_railroad => {
+          :mortgage_value => 200
+        },
+        :b_and_o_railroad => {
+          :mortgage_value => 400
+        },
+        :shortline_railroad => {
+          :mortgage_value => 800
+        }
+      }
+    }
   }
-  monopoly[:railroads][:names][:pennsylvania_railroad] = {
-    mortgage_value: 200
-  }
-  monopoly[:railroads][:names][:b_and_o_railroad] = {
-    mortgage_value: 400
-  }
-  monopoly[:railroads][:names][:shortline_railroad] = {
-    mortgage_value: 800
-  }
-
-  pp monopoly
-  monopoly
 end
-
-monopoly_with_fourth_tier
